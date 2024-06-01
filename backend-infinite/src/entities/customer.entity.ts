611e8ac32cedaf9ff7624d325054b8ce517107c0
Juan11042001 <alejandro.dealmada10@gmail.com>
2024-06-01 13:59:32 -0500

    tablas book,customer,loan

--- backend-infinite/src/entities/customer.entity.ts
+++ backend-infinite/src/entities/customer.entity.ts
@@ -1,19 +1,39 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  OneToMany,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+import { Loan } from './loan.entity';
+import { User } from './user.entity';
+import { City } from './city.entity';
+import { Gender } from './gender.entity';
 
 @Entity('customer')
-export class CustomerEntity {
-@PrimaryGeneratedColumn('uuid')
-id:string;
+export class Customer {
+  @PrimaryGeneratedColumn()
+  idcustomer: number;
 
-@Column({type:'varchar',name:'first_najjjjjjjj', length:'10', comment:'Nombres del usuario'})
-name:string;
+  @Column({ type: 'varchar', length: 20 })
+  name: string;
 
-@Column({type:'integer', name:'age'})
-age:number;
+  @Column({ type: 'varchar', length: 20 })
+  lastname: string;
 
-@Column({ type:'boolean', name:'state'})
-state:boolean;
+  @ManyToOne(() => Gender, (gender) => gender.customers)
+  @JoinColumn({ name: 'idgender' })
+  gender: Gender;
 
+  @ManyToOne(() => City, (city) => city.customers)
+  @JoinColumn({ name: 'idcty' })
+  city: City;
 
+  @ManyToOne(() => User, (user) => user.customers)
+  @JoinColumn({ name: 'iduser' })
+  user: User;
 
+  @OneToMany(() => Loan, (loan) => loan.customer)
+  loans: Loan[];
 }
