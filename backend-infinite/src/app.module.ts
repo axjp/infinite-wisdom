f14a3cbef59c1baa7762f66d0db9a173149c1616
Juan11042001 <alejandro.dealmada10@gmail.com>
2024-06-01 17:54:05 -0500

    tablas y provider mas cambios de fron actualizacion 2.0

--- backend-infinite/src/app.module.ts
+++ backend-infinite/src/app.module.ts
@@ -1,11 +1,57 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { DatabaseModule } from './database/database.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './typeorm.config';
+import { User } from './entities/user.entity';
+import { Role } from './entities/role.entity';
+import { Customer } from './entities/customer.entity';
+import { Administrator } from './entities/administrator.entity';
+import { Gender } from './entities/gender.entity';
+import { City } from './entities/city.entity';
+import { Book } from './entities/book.enitity';
+import { Download } from './entities/download.entity';
+import { Category } from './entities/category.entity';
+import { CategoryBook } from './entities/categorybook.entity';
+import { Loan } from './entities/loan.entity';
 
 @Module({
-  imports: [DatabaseModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'yourpassword',
+      database: 'yourdatabase',
+      entities: [
+        User,
+        Role,
+        Customer,
+        Administrator,
+        Gender,
+        City,
+        Book,
+        Download,
+        Category,
+        CategoryBook,
+        Loan
+      ],
+      synchronize: true,
+    }),
+    TypeOrmModule.forFeature([
+      User,
+      Role,
+      Customer,
+      Administrator,
+      Gender,
+      City,
+      Book,
+      Download,
+      Category,
+      CategoryBook,
+      Loan
+    ]),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
