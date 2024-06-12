f72150547a1f3d7b2adc458df089d38c52e6502f
axjp <119636911+axjp@users.noreply.github.com>
2024-06-12 13:13:11 -0500

    book

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,10 @@ import { ReviewsListComponent } from './modules/reviews/reviews-list/reviews-lis
 import { LoanComponent } from './modules/loan/component/loan.component';
 
 const routes: Routes = [
+  {
+    path: 'book',
+    loadChildren: () => import('./modules/book/book.module').then(m => m.BookModule),
+  },
   { path: '', component: MainComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register/:idcustomer', component: RegisterComponent },
