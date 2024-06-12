220115bb7b933937bc32d99f818714b728537539
Bryan0695 <bdf.gallegos@yavirac.edu.ec>
2024-06-12 13:15:44 -0500

    .

--- src/app/modules/administrator/administrator-form/administrator-form.component.ts
+++ src/app/modules/administrator/administrator-form/administrator-form.component.ts
@@ -1,6 +1,6 @@
 import { Component, inject } from '@angular/core';
-import { FormControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { AbstractControl,FormControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { __values } from 'tslib';
 
 @Component({
   selector: 'app-administrator-form',
@@ -10,26 +10,28 @@ import { Router } from '@angular/router';
 export class AdministratorFormComponent {
   private formBuilder: FormBuilder = inject(FormBuilder);
 
-  protected nameControl: FormControl = new FormControl<string>('');
-  protected lastnameControl: FormControl = new FormControl<string>('');
-  protected numberControl: FormControl = new FormControl<string>('');
-  protected birthdayControl: FormControl = new FormControl<string>('');
-  protected stateControl: FormControl = new FormControl<string>('');
+  //protected nameControl: FormControl = new FormControl<string>('');
+  //protected lastnameControl: FormControl = new FormControl<string>('');
+  //protected numberControl: FormControl = new FormControl<number>('');
+  //protected birthdayControl: FormControl = new FormControl<date>('');
+  //protected stateControl: FormControl = new FormControl<boolean>('');
 
   protected form: FormGroup;
-  numberField: any;
+
   nameField: any;
+  lastNameField:any;
+  numberField: any;
   birthdateField: any;
   booleanoField: any;
 
-  constructor(private router: Router) {
-    this.form = this.buildForm;
+  constructor() {
+    this.form = this.buildForm();
   }
 
-  get buildForm(): FormGroup {
+  private buildForm(): FormGroup {
     return this.formBuilder.group({
       name: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(50)]],
-      lastname: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(50)]],
+      lastName: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(50)]],
       number: [null, [Validators.min(1), Validators.required]],
       birthdate: [[], [Validators.required]],
       booleano: [false, Validators.requiredTrue]
--- src/app/modules/administrator/administrator.module.ts
+++ src/app/modules/administrator/administrator.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
+
 import { AdministratorRoutingModule } from './administrator-routing.module';
 import { AdministratorFormComponent } from './administrator-form/administrator-form.component';
 import { AdministratorListComponent } from './administrator-list/administrator-list.component';
